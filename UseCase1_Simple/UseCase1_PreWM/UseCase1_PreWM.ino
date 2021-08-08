ea4cef3f990476ae404d0f05f5e3364fe6ea8e11
witnessmenow <ecenuig@gmail.com>
2021-08-08 23:51:00 +0100

    Adding Use Case 1 Examples

---
+++ UseCase1_Simple/UseCase1_PreWM/UseCase1_PreWM.ino
@@ -0,0 +1,32 @@
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ UseCase1_Simple/UseCase1_WithWM/UseCase1_WithWM.ino
@@ -0,0 +1,30 @@
+#include <WiFiManager.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  WiFiManager wifiManager;
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if(!wifiManager.autoConnect()) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.restart();
+    delay(1000);
+  } 
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
